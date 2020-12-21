0d68d88ff3d73015b208cb57220babd764d41427
Cedric Perauer <40869163+Cedric-Perauer@users.noreply.github.com>
2020-12-21 01:10:55 +0100

    Add files via upload

---
+++ CNNs/Detectron2_utils/test.py
@@ -0,0 +1,147 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import argparse
+import glob
+import multiprocessing as mp
+import os
+import time
+import cv2
+import tqdm
+
+from detectron2.config import get_cfg
+from detectron2.data.detection_utils import read_image
+from detectron2.utils.logger import setup_logger
+
+from predictor import VisualizationDemo
+import pandas as pd
+
+# constants
+WINDOW_NAME = "COCO detections"
+
+
+def setup_cfg(args):
+    # load config from file and command-line arguments
+    cfg = get_cfg()
+    # To use demo for Panoptic-DeepLab, please uncomment the following two lines.
+    # from detectron2.projects.panoptic_deeplab import add_panoptic_deeplab_config  # noqa
+    # add_panoptic_deeplab_config(cfg)
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    # Set score_threshold for builtin models
+    cfg.MODEL.RETINANET.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.confidence_threshold
+    cfg.MODEL.PANOPTIC_FPN.COMBINE.INSTANCES_CONFIDENCE_THRESH = args.confidence_threshold
+    cfg.freeze()
+    return cfg
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Detectron2 demo for builtin configs")
+    parser.add_argument(
+        "--config-file",
+        default="configs/quick_schedules/mask_rcnn_R_50_FPN_inference_acc_test.yaml",
+        metavar="FILE",
+        help="path to config file",
+    )
+    parser.add_argument("--webcam", action="store_true", help="Take inputs from webcam.")
+    parser.add_argument("--video-input", help="Path to video file.")
+    parser.add_argument(
+        "--input",
+        nargs="+",
+        help="A list of space separated input images; "
+        "or a single glob pattern such as 'directory/*.jpg'",
+    )
+    parser.add_argument(
+        "--output",
+        help="A file or directory to save output visualizations. "
+        "If not given, will show output in an OpenCV window.",
+    )
+
+    parser.add_argument(
+        "--confidence-threshold",
+        type=float,
+        default=0.5,
+        help="Minimum score for instance predictions to be shown",
+    )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options using the command-line 'KEY VALUE' pairs",
+        default=[],
+        nargs=argparse.REMAINDER,
+    )
+    return parser
+
+
+img_path = os.path.expanduser("~/detectron2/demo/imgs/") 
+img_path_new = os.path.expanduser("~/detectron2/demo/out_imgs/") 
+imgs = os.listdir(img_path)
+imgs.sort()
+
+#dataframe numbers
+number_detections = []
+cls = []
+scores = []
+
+
+if __name__ == "__main__":
+    mp.set_start_method("spawn", force=True)
+    args = get_parser().parse_args()
+    setup_logger(name="fvcore")
+    logger = setup_logger()
+    logger.info("Arguments: " + str(args))
+
+    cfg = setup_cfg(args)
+
+    demo = VisualizationDemo(cfg)
+
+    if args.input:
+        if len(args.input) == 1:
+            args.input = glob.glob(os.path.expanduser(args.input[0]))
+            print(args.input[0]) 
+            assert args.input, "The input path(s) was not found"
+        for img_name in tqdm.tqdm(imgs, disable=not args.output):
+            # use PIL, to be consistent with evaluation
+            path = img_path + img_name 
+            print(path) 
+            img = read_image(path, format="BGR")
+            start_time = time.time()
+            predictions, visualized_output = demo.run_on_image(img)
+            logger.info(
+                "{}: {} in {:.2f}s".format(
+                    path,
+                    "detected {} instances".format(len(predictions["instances"]))
+                    if "instances" in predictions
+                    else "finished",
+                    time.time() - start_time,
+                )
+            )
+            number_detections.append(len(predictions["instances"]))
+            classes = predictions["instances"].pred_classes.cpu().numpy().tolist()  
+            scrs = predictions["instances"].scores.cpu().numpy().tolist() 
+            cls.append(classes) 
+            scores.append(scrs) 
+                          
+
+            if args.output:
+                if os.path.isdir(args.output):
+                    assert os.path.isdir(args.output), args.output
+                    out_filename = os.path.join(args.output, os.path.basename(path))
+                else:
+                    assert len(args.input) == 1, "Please specify a directory with args.output"
+                    out_filename = args.output
+                visualized_output.save(out_filename)
+            else:
+                #cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
+                img = visualized_output.get_image()[:, :, ::-1]
+                #cv2.imshow(WINDOW_NAME,img)
+                cv2.imwrite(img_path_new + img_name,img) 
+                print("yes") 
+                #if cv2.waitKey(0) == 27:
+                #    break  # esc to quit
+#Create pandas dataframe 
+d = {"Number_Detections": number_detections,"Classes":cls,"scores":scores} 
+df = pd.DataFrame(data=d)
+
+df.to_csv(r'export_dataframe.csv', index = False, header=True)
+
+
+
